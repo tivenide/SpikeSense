2ab7af8cd2d5170d31325bc9f30f8bc0d5e11b14
tivenide <tivenide@gmail.com>
2023-07-03 18:54:32 +0200

    adding several evaluation and helper methods

--- evaluation.py
+++ evaluation.py
@@ -1,3 +1,83 @@
+
+def save_spiketrains_to_hdf5(input_array, filename):
+    import h5py
+    with h5py.File(filename, 'w') as file:
+        st_group = file.create_group('st')
+        for i, values in enumerate(input_array):
+            electrode_name = f'electrode_{i}'
+            if len(values) > 0:
+                st_group.create_dataset(electrode_name, data=values)
+            else:
+                # If values is an empty list or array, create an empty dataset
+                st_group.create_dataset(electrode_name, shape=(0,))
+
+        # Optional:
+        # st_group.attrs['description'] = 'electrode values'
+
+        print(f"Data saved to {filename} successfully.")
+
+def read_spiketrains_from_hdf5(filename):
+    import h5py
+    import numpy as np
+    data = []
+    with h5py.File(filename, 'r') as file:
+        st_group = file['st']
+        electrode_keys = sorted(st_group.keys(), key=lambda x: int(x.split('_')[1]))
+        electrodes = len(electrode_keys)
+        data = np.empty(electrodes, dtype=object)
+        for i, electrode_key in enumerate(electrode_keys):
+            dataset = st_group[electrode_key]
+            if dataset.shape[0] > 0:
+                data[i] = dataset[:]
+            else:
+                data[i] = np.array([])
+    return data
+
+
+def extract_timestamps(input_array):
+    """
+    extract timestamps of preprocessed array
+    :param input_array: input array with dimensions (electrodes, 3, timestamps) with
+        second dimension: (0: values, 1: labels, 2: timestamps)
+    :return: spiketrains in standard array format
+    """
+    import numpy as np
+    sensors = input_array.shape[0]
+    output_array = []
+
+    for i in range(sensors):
+        labels = input_array[i, 1]
+        timestamps = input_array[i, 2]
+
+        # Extract timestamps where label is 1
+        timestamps_1 = [timestamp for timestamp, label in zip(timestamps, labels) if label == 1]
+
+        if len(timestamps_1) > 0:
+            output_array.append(np.array(timestamps_1))
+        else:
+            output_array.append(np.array([]))
+
+    return np.array(output_array, dtype=object)
+
+def get_ground_truth_spiketrains_in_standard_array_format(path, threshold=100):
+    """
+    Preprocessing pipeline for one recording (without windowing) and returns ground truth spiketrains.
+    :param path: path to recording file
+    :param threshold: The maximum distance between an electrode location and a neuron location for them
+            to be considered a match.
+    :return: spiketrains in standard array format
+    """
+    from utilities import prepro
+    any = 'any'
+    prepro_obj = prepro(path_to_original_recordings=any, path_to_target_prepro_results=any, threshold=any, windowsize_in_sec=any, step_size=any, feature_calculation=any, scaler_type=any, sampling_strategy=any)
+    signal_raw, timestamps, ground_truth, electrode_locations, neuron_locations = prepro_obj.import_recording_h5(path=path)
+    labels_of_all_spiketrains = prepro_obj.create_labels_of_all_spiketrains(ground_truth, timestamps)
+    assignments = prepro_obj.assign_neuron_locations_to_electrode_locations(electrode_locations, neuron_locations, threshold=threshold)
+    merged_data = prepro_obj.merge_data_to_location_assignments(assignments, signal_raw.transpose(), labels_of_all_spiketrains, timestamps)
+    output = extract_timestamps(merged_data)
+    return output
+
+
 def calculate_nearest_distances(gt, det):
     """
     Calculates the distances to the nearest timepoints between the two input arrays.
@@ -20,11 +100,11 @@ def calculate_nearest_distances(gt, det):
         distances.append(nearest_distance)
     return distances
 
-def calculate_metrics_for_different_thresholds(pos_gt, pos_det, plot=False):
+def calculate_metrics_for_different_gt_temporal_assignment_thresholds(pos_gt, pos_det, plot=False):
     """
     Calculates and plots (optionally) TruePositive (TP), FalsePositive (FP) and FalseNegative (FN) with different
     assignment thresholds between two timeseries vectors. The function helps to identify the right threshold, when the
-    detected timepoint does not match exactly the ground timepoint.
+    detected timepoint does not match exactly the ground truth timepoint.
     :param pos_gt: sorted array with ground truth timepoints of the positive class (minimum size: 2 items)
     :param pos_det: sorted array with detected timepoints of the positive class (minimum size: 2 items)
     :param plot: boolean. Default: False
@@ -49,12 +129,108 @@ def calculate_metrics_for_different_thresholds(pos_gt, pos_det, plot=False):
         plt.plot(thresholds, tps, label='TPs')
         plt.plot(thresholds, fps, label='FPs')
         plt.plot(thresholds, fns, label='FNs')
-        plt.xlabel('Threshold')
+        plt.xlabel('Threshold in sec')
         plt.ylabel('Count')
-        plt.title('Assignment to metric values for different thresholds')
+        plt.title('TPs, FPs, FNs in dependency of gt_temporal_assignment_thresholds')
         plt.grid(True)
         plt.legend()
         plt.show()
+
+    return tps, fps, fns, thresholds
+
+def calculate_metrics_for_mea_chip(gt_spiketrains, det_spiketrains, threshold=0.002, verbose=False):
+    """
+    Calculates TruePositive (TP), FalsePositive (FP) and FalseNegative (FN) for mea-recording
+    :param gt_spiketrains: ground truth spiketrains (positive class)
+    :param det_spiketrains: detection spiketrains (positive class)
+    :param threshold: temporal distance threshold for true positive (tp) assignment. If this is set too high,
+        it can lead to negative values in other metrics, as these are derived from tp. Default: 0.002 sec
+    :param verbose: boolean. Prints results per electrode. Default: False
+    :return: Summation of all TPs, FPs and FNs (total_tps, total_fps, total_fns)
+    """
+    import numpy as np
+    print('calculate metrics for mea chip')
+
+    tps = []
+    fps = []
+    fns = []
+    tns = []
+    for i in range(gt_spiketrains.shape[0]):
+        gt_spiketrain = gt_spiketrains[i]
+        det_spiketrain = det_spiketrains[i]
+
+        if gt_spiketrain.size == 0 and det_spiketrain.size >=0:
+            fp = det_spiketrain.size
+            fps.append(fp)
+            if verbose:
+                print(f'el:{i}\tfp:{det_spiketrain.size}')
+        elif gt_spiketrain.size >= 0 and det_spiketrain.size == 0:
+            fn = gt_spiketrain.size
+            fns.append(fn)
+            if verbose:
+                print(f'el:{i}\tfn:{gt_spiketrain.size}')
+        elif gt_spiketrain.size == 0 and det_spiketrain.size == 0:
+            tn = 'not calculated' # sample_frequency * recording_duration
+            if verbose:
+                print(f'el:{i}\ttn:{tn}')
+        elif gt_spiketrain.size >= 0 and det_spiketrain.size >= 0:
+            if verbose:
+                print(f'el:{i}\there we have to calculate')
+            if gt_spiketrain.size >= 2 and det_spiketrain.size >= 2:
+                if verbose:
+                    print(f'el:{i}\there we can calculate')
+                pos_distances = calculate_nearest_distances(gt=gt_spiketrain, det=det_spiketrain)
+                tp = np.sum(np.array(pos_distances) <= threshold)
+                fp = det_spiketrain.size - tp
+                fn = gt_spiketrain.size - tp
+                if verbose:
+                    print(f'el:{i}\ttp:{tp}\tfp:{fp}\tfn:{fn}')
+                tps.append(tp)
+                fps.append(fp)
+                fns.append(fn)
+    total_tps = np.sum(np.array(tps))
+    total_fps = np.sum(np.array(fps))
+    total_fns = np.sum(np.array(fns))
+    print('totals:')
+    print(f'tp: {total_tps}\tfp: {total_fps}\tfn:{total_fns}')
+    return total_tps, total_fps, total_fns
+
+
+def calculate_metrics_for_different_gt_location_assignment_thresholds(path_to_h5_gt_spiketrains, path_to_h5_det_spiketrains, threshold=0.002):
+    """
+    Calculates and plots TruePositive (TP), FalsePositive (FP) and FalseNegative (FN) with different neuron-electrode
+    assignment thresholds between two spiketrains sets. The function helps to identify the right threshold.
+    :param path_to_h5_gt_spiketrains: path to ground truth spiketrains in standard array format
+    :param path_to_h5_det_spiketrains: path to detection spiketrains in standard array format
+    :param threshold: temporal distance threshold for true positive (tp) assignment. If this is set too high,
+        it can lead to negative values in other metrics, as these are derived from tp. Default: 0.002 sec
+    :return:
+    """
+    import numpy as np
+    import matplotlib.pyplot as plt
+    tps = []
+    fps = []
+    fns = []
+    thresholds = np.linspace(start=10, stop=200, num=10)
+
+    for gt_location_assignment_threshold in thresholds:
+        gt_spiketrains = get_ground_truth_spiketrains_in_standard_array_format(path_to_h5_gt_spiketrains, threshold=gt_location_assignment_threshold)
+        det_spiketrains = read_spiketrains_from_hdf5(path_to_h5_det_spiketrains)
+        total_tps, total_fps, total_fns = calculate_metrics_for_mea_chip(gt_spiketrains=gt_spiketrains, det_spiketrains=det_spiketrains, threshold=threshold)
+        tps.append(total_tps)
+        fps.append(total_fps)
+        fns.append(total_fns)
+
+    plt.plot(thresholds, tps, label='TPs')
+    plt.plot(thresholds, fps, label='FPs')
+    plt.plot(thresholds, fns, label='FNs')
+
+    plt.xlabel('Threshold in µm')
+    plt.ylabel('Count')
+    plt.title('TPs, FPs, FNs in dependency of gt_location_assignment_threshold')
+    plt.grid(True)
+    plt.legend()
+    plt.show()
 
     return tps, fps, fns, thresholds
 

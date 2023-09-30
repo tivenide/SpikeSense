156695a5d3c83a637f20370fa0229c5e7480cc1e
tivenide <tivenide@gmail.com>
2023-09-30 21:03:11 +0200

    development_workflow_quick inside main()

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 
+def main():
+    from development.development_workflow_quick import development_workflow_quick
+    development_workflow_quick()
+
+
 if __name__ == '__main__':
     print('Welcome to SpikeSense')
 
-
+    main()
 
 
 
